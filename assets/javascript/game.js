4975189af06a28239a992ee3a3878bc2aa1f324b
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-10-04 16:11:39 -0500

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,12 @@
 var won = 0;
 var lost = 0;
 var guessesLeft = 9;
+var soFar = [];
 var usedLetters = [];
+var ranLetter = ranLetter;
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//GENERATE COMPUTER CHOICES
+//GENERATE COMPUTER RANDOM CHOICES
 function jsGuess() {
     ranLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(ranLetter);
@@ -16,22 +18,23 @@ jsGuess();
 //Capturing player input
 //.onkeyup captures the players input as playerGuess
 document.onkeyup = function (event) {
-    var sofar = event.key;
+    var soFar = event.key;
+    console.log(soFar);
     //stats();
 }
 //Handeling guesses - output
 //This "if" prevents a letter selected a 2nd time from being written over
-if (usedLetters.indexOf(sofar) >= 0) {
+if (usedLetters.indexOf(soFar) >= 0) {
 
 } else {
     //this pushes the players incorrect guess to the usedLetters array
-    usedLetters.push(sofar);
-    document.getElementById("sofar").innerHTML = usedLetters;
+    usedLetters.push(soFar);
+    document.getElementById("soFar").innerHTML = usedLetters;
     //console.log(usedLetters);
 }
 //Handeling guesses
 //Test if players guess does not equal ranLetter and decriments guessLeft
-if (sofar !== ranLetter) {
+if (soFar !== ranLetter) {
     guessesLeft--;
 } else {
     won++;
@@ -41,7 +44,7 @@ if (sofar !== ranLetter) {
     alert("You did it!");
 }
 //When guessLeft equals zero, lost is incrimented by 1; guessLeft is reset to 9
-if (guessesLeft === 0){
+if (guessesLeft === 0) {
     lost++;
     usedLetters = [];
     guessesLeft = 9;
@@ -53,7 +56,8 @@ if (guessesLeft === 0){
 //these statements write the value/scores generated to the HTML
 document.getElementById("wins").innerHTML = won;
 document.getElementById("losses").innerHTML = lost;
-document.getElementById("sofar").innerHTML = sofar;
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+document.getElementById("soFar").innerHTML = soFar;
 
 
 
